4d117f91cf12bc9b1e60603764312248d1671d84
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-09 15:48:36 -0600

    cliente entity

---
+++ src/main/java/gm/zona_fit/modelo/Cliente.java
@@ -0,0 +1,22 @@
+package gm.zona_fit.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data // Esto genera los getter y setter de cada uno de los valores (Me recuerda a kotlin)
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // Se utiliza la clase porque para jpa es más facil enternder el valor por default de null
+    private String nombre;
+    private String apellido;
+    private Integer membresia;
+}
