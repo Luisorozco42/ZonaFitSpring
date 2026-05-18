71e78309bd19acea11c537675089a238d230e691
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-17 19:09:20 -0600

    showing clients and creating a client

--- src/main/java/gm/zona_fit/controler/IndexControlador.java
+++ src/main/java/gm/zona_fit/controler/IndexControlador.java
@@ -3,8 +3,11 @@ package gm.zona_fit.controler;
 import gm.zona_fit.model.Cliente;
 import gm.zona_fit.service.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,7 @@ public class IndexControlador {
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
     private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+    private Cliente clienteSeleccionado;
 
     //El constructor vacio lo manda a llamar por default jsf
     //Este metodo es despues de que es llamado
@@ -29,8 +33,28 @@ public class IndexControlador {
         cargarDatos();
     }
 
-    private void cargarDatos() {
+    public void cargarDatos() {
         this.clientes = clienteServicio.listarClientes();
         this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+
+    public void agregarCliente() {
+        this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente() {
+        logger.info("Cliente a guardar: " + this.clienteSeleccionado);
+        //Agregar
+        if (this.clienteSeleccionado.getId() == null) {
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cliente Agregado"));
+            //Ocultar la ventana modal
+            PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+            //Actualizar la ventana usando ajax
+            PrimeFaces.current().ajax().update("forma-clientes:mensajes", "forma-clientes:clientes-tabla");
+            //reset del objeto cliente seleccionado
+            this.clienteSeleccionado = null;
+        }
     }
 }
